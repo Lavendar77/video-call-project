638f88ce802480e10ba61594a075be41d16d182b
Adeyinka Adefolurin <folurinyinka@gmail.com>
2022-01-05 11:37:17 +0100

    add register link to login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -26,7 +26,11 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+            <Link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                New account?
+            </Link>
+
+            <Link v-if="canResetPassword" :href="route('password.request')" class="ml-4 underline text-sm text-gray-600 hover:text-gray-900">
                 Forgot your password?
             </Link>
 
